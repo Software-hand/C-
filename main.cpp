8cfdf47281df45ec6b09514533e1ee5fa1f37962
软件手 <471583744@QQ.COM>
2022-05-05 14:41:04 +0800

    合并测试
    合并测试

--- main.cpp
+++ main.cpp
@@ -1,21 +1,22 @@
 /*
-2.3.1 ʵ������ ������﷨��ϰ
-	��д����Ҫ��
-	�ٽ����ݳ�Ա��Ϊ˽�еģ�
-	�ڽ����������Ĺ��ܸ�Ϊ�ɳ�Ա����ʵ�֣�
-	���������ڶ����Ա������
-	Ȼ���������г��������ʲô��ԱӦָ��Ϊ���õ�?ʲô��ԱӦָ��Ϊ˽�е�? ʲô������÷������ж���? ʲô������������ⶨ��?
+2.3.1 实验内容 类基本语法练习
+	改写程序，要求：
+	①将数据成员改为私有的；
+	②将输入和输出的功能改为由成员函数实现；
+	③在类体内定义成员函数。
+	然后编译和运行程序。请分析什么成员应指定为公用的?什么成员应指定为私有的? 什么函数最好放在类中定义? 什么函数最好在类外定义?
+	
 */
 
 #include <iostream> 
 using namespace std; 
-class Time // ����Time�� 
+class Time // 定义Time类 
 {
-private:// ���ݳ�ԱΪ˽�е� 
+private:// 数据成员为私有的 
 	int hour; 
 	int minute; 
 	int sec; 
-public: // ���ݳ�Ա����Ա����Ϊ���õ� 
+public: // 数据成员、成员函数为公用的 
 	void input(); 
 	void output(); 
 };
@@ -28,11 +29,11 @@ void Time::output()
 	cout << hour << ":" << minute << ":" << sec << endl; 
 }
 
-// ���� ʱ����
+// 输入 时分秒
 int main() 
 {
-	Time t1; //����t1ΪTime����� 
-	t1.input(); //�����趨��ʱ�� 
+	Time t1; //定义t1为Time类对象 
+	t1.input(); //输入设定的时间 
 	t1.output(); 
 	system("pause"); 
 	return 0; } 
